9e2b8c0db40ba40d73c9e341c9fe75d93e7813d9
Narendra <narendradmirer145@gmail.com>
2022-03-07 12:25:02 +0530

    Added support for map, set, unordered_set in ostream

--- C++/ostream.cpp
+++ C++/ostream.cpp
@@ -40,6 +40,48 @@ ostream& operator << (ostream &out, unordered_map<K, V> &mp){
     return out;
 }
 
+// for custom hasher
+template<typename K, typename V, typename Hasher>
+ostream& operator << (ostream &out, unordered_map<K, V, Hasher> &mp){
+    for(auto val: mp){
+        out << val << " ";
+    }
+    return out;
+}
+
+template<typename K, typename V>
+ostream& operator << (ostream &out, map<K, V> &mp){
+    for(auto val: mp){
+        out << "[" << val.first << " : " << val.second << "]\n";
+    }
+    return out;
+}
+
+template<typename T>
+ostream& operator << (ostream &out, set<T> &s){
+    for(auto val: s){
+        out << val << " ";
+    }
+    return out;
+}
+
+template<typename T>
+ostream& operator << (ostream &out, unordered_set<T> &s){
+    for(auto val: s){
+        out << val << " ";
+    }
+    return out;
+}
+
+// for custom hasher
+template<typename T, typename Hasher>
+ostream& operator << (ostream &out, unordered_set<T, Hasher> &s){
+    for(auto val: s){
+        out << val << " ";
+    }
+    return out;
+}
+
 struct Node{
     int key;
     int value;
@@ -47,11 +89,34 @@ struct Node{
         out << "(" << node.key << " " << node.value << ")";
         return out;
     }
+    // for working with set
+    bool operator < (const Node &other) const{
+        if(key == other.key){
+            return value < other.value;
+        }
+        return key < other.key;
+    }
+    // for working with unordered set
+    bool operator == (const Node &other) const{
+        return key == other.key && value == other.value;
+    }
 };
 
+struct NodeHasher{
+    size_t operator()(const Node &node) const{
+        return std::hash<int>()(node.key) ^ (std::hash<int>()(node.value) << 1);
+    }
+};
+
+
 void test(){
-    vector<Node> arr = {{1, 2}, {4, 3}, {10, 20}};
-    cout << arr << "\n";
+    unordered_map<Node, int, NodeHasher> mp;
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=5; j++){
+            mp[{i, j}] = (i-1)*10 + j;
+        }
+    }
+    cout << mp << "\n";
 }
 
 int main(){
